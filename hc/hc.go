391f0362a930fc0ee1f3979fc5b57a246b744c84
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-11-02 23:58:46 -0300

    Add hierarchical clustering contract

--- hc/hc.go
+++ hc/hc.go
@@ -7,9 +7,19 @@ import clf "github.com/Giulianos/ml-tp4/classifier"
 type HC struct {
 }
 
+// New creates a new Hierarchical Clustering classifier
+func New() HC {
+	return HC{}
+}
+
 // Classify classifies an example
 func (hc HC) Classify(e clf.Example) string {
 	return ""
+}
+
+// Fit trains the classifier
+func (hc *HC) Fit(examples []clf.Example) {
+
 }
 
 // GetTargetAttribute returns the target

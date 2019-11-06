1a24f9ab090cdd04c433b6df33428d3146eecefe
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-11-06 00:04:35 -0300

    Add confusion matrix plotter

---
+++ plotters/cf.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+
+df = pd.read_csv(sys.stdin)
+
+df = pd.DataFrame([
+    [df['tn'][0], df['fp'][0]],
+    [df['fn'][0], df['tp'][0]]
+    ], columns=['F','T'], index=['F', 'T'])
+
+cf = sns.heatmap(df, annot=True, cmap='Blues', fmt='g')
+plt.ylabel('True value')
+plt.xlabel('Predicted value')
+plt.title(sys.argv[1])
+plt.show()
